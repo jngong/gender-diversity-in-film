832389f920adeddf4e34b44b8438bc600d930ca9
jngong <34215359+jngong@users.noreply.github.com>
2020-01-24 12:34:56 -0500

    set up env file

--- film-diversity/src/services/api-helper.js
+++ film-diversity/src/services/api-helper.js
@@ -1,6 +1,8 @@
 import axios from "axios"
+import dotenv from "dotenv"
+dotenv.config()
 
-const api_key = ''
+const api_key = process.env.REACT_APP_TMDB_API_KEY
 const URL_Search_TMDB = ''
 const URL_Get_Movie_TMDB = ''
 const URL_Bechdel_Test = ''
