05db94fcadedd17e9d68ea829f80462ea6748540
jngong <34215359+jngong@users.noreply.github.com>
2020-01-24 11:31:13 -0500

    created Header and Footer component boilerplates

---
+++ film-diversity/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer>
+            <p>2020 | Built with React | Jennifer Gong</p>
+        </footer>
+    )
+}
+
+export default Footer
