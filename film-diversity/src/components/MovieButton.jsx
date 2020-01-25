a84fd4688d0e9c1bb265e0ece78be3f4401faedf
jngong <34215359+jngong@users.noreply.github.com>
2020-01-25 08:40:49 -0500

    movie button displays poster and title

--- film-diversity/src/components/MovieButton.jsx
+++ film-diversity/src/components/MovieButton.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 
 function MovieButton(props) {
-    const {id, title} = props.movie
+    const {poster_path, title} = props.movie
+    const imageURL = 'https://image.tmdb.org/t/p/w185/'
+
     return(
             <div className="movie-button">
+                <img src={`${imageURL}${poster_path}`} alt={title}/>
                 <h2>{title}</h2>
             </div>        
     )
